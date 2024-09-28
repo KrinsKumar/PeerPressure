46a28f6e46dfbb9af394ae0ebf38d429f9523aad
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-09-28 00:12:49 -0400

    feat: add development environment configuration

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Hack the Hill";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+            corepack_22
+          ];
+        };
+      });
+}
